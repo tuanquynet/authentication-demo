ea144a2656920cc5c6505fc3b47ae5b2f99b2b6b
Thanh Tran <hoangthanh28@gmail.com>
2019-08-20 21:44:13 +0700

    - Remove authentication middleware

--- src/Demo.BasicAuthentication/Startup.cs
+++ src/Demo.BasicAuthentication/Startup.cs
@@ -26,10 +26,10 @@ namespace Demo.BasicAuthentication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddControllers(options =>
-            // {
-            //     options.Filters.Add(typeof(AuthenticationValidation));
-            // });
+            services.AddControllers(options =>
+            {
+                // options.Filters.Add(typeof(AuthenticationValidation));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,8 +43,6 @@ namespace Demo.BasicAuthentication
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
